913e50beb7385278a6754046ea94acaeda074592
Андрей <andrey.solovey.2013@bk>
2020-05-05 23:24:50 +0300

    homework

---
+++ dz2.py
@@ -0,0 +1,7 @@
+n = int(input())
+x = []
+for i in range(n):
+    x.append(i)
+x.sort(reverse=True)
+print(x[0])
+print(x[1])
---
+++ dz3.py
@@ -0,0 +1,11 @@
+import random
+n = int(input())
+x = []
+for i in range(n):
+    x.append(random.randint(-5, 5))
+for i in x:
+    if i == 0:
+        x.insert(i)
+    else:
+        pass
+print(x)
